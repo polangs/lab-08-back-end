97a23e21e59acbc6eb4e32e3fe5e2221e2d034e3
paulacruz <cruz.amp12@gmail.com>
2019-06-17 13:33:22 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# City Explorer - lab-08 APIs
+
+## Time Estimate
+
+Lab 08 Feature #1-#4
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+**Authors**
+
+* **Trae Bennett** - [GitHub Profile](https://github.com/TraeBenn22)
+* **Paula Cruz** - [GitHub Profile](https://github.com/polangs)
+* **Matthew Heyert** - [GitHub Profile](https://github.com/heyerthb)
+* **Na'ama Bar-Ilan** - [GitHub Profile](https://github.com/NaamaBarIlan)
+
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+In the City Explorer lab we'll be building a stand-alone back end that will interact with a static front end. When the user selects a location, we'll the app will present related info, such as weather. 
+
+## Getting Started
+A user will need to have a link to our deployed site. 
+
+## Architecture
+TBA
+
+## Change Log
+TBA
+
+## Built With
+
+* [Visual Studio Code](https://code.visualstudio.com/) - The code editor.
+* [GitHub](https://github.com/) -  Version control.
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ server.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// enviroment variables
+require('dotenv').config();
+
+// Application dependencies
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+const pg = require ('pg');
+
+// database set up
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+// Application set up
+const app = express();
+const PORT = process.env.PORT;
+app.use(cors());
+
+
+// get location
+
+// routes
+app.get('/location', handleLocation);
+app.get('/weather', handleWeather);
+app.get('/events', handleEvents);
+
+// internal modules
+const getLocation = require('./modules/locations');
+const getForecasts = require('./modules/weather');
+const getEvents = require('./modules/events');
+
+
+// route handlers
+function handleLocation(req, res) {
+
+  getLocation(req.query.data, client, superagent)
+    .then(location => res.send(location))
+    .catch(error => handleError(error, res));
+}
+
+function handleWeather(req, res) {
+  console.log('************* handle weather', req.query.data);
+
+  getForecasts(req.query.data.latitude, req.query.data.longitude, client, superagent)
+    .then(forecasts => res.send(forecasts))
+    .catch(error => handleError(error, res));
+}
+
+function handleEvents(req, res) {
+
+  getEvents(req.query)
+    .then(data => res.send(data))
+    .catch(error => handleError(error))
+
+
+  getEvents(req.query.data.formatted_query, client, superagent)
+    .then(events => res.send(events))
+    .catch(error => handleError(error, res));
+}
+
+function handleError(error, response) {
+  console.error(error);
+  response.status(500).send('ERROR');
+}
+
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
